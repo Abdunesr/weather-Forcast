9f8af0292503ff1dbae7829a0e4d611e89dee8ae
Abdunesr <nesredinabdelah33@gmail.com>
2024-11-25 00:32:08 +0300

    Adding the featching api and logic using promise in handle featch function

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,34 @@ class App extends React.Component {
     super(props);
 
     this.state = { location: "lisbon" };
-    this.handleFeatching = this.handleFeatching.bind(this);
+    this.FetchWeather = this.FetchWeather.bind(this);
   }
-  handleFeatching() {
-    console.log("i am featching ");
+  async FetchWeather(location) {
+    try {
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      console.log(`${name} ${convertToFlag(country_code)}`);
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      console.log(weatherData.daily);
+    } catch (err) {
+      console.err(err);
+    }
   }
+
   render() {
     return (
       <div className="app">
@@ -22,7 +45,7 @@ class App extends React.Component {
             onChange={(e) => this.setState({ location: e.target.value })}
           />
         </div>
-        <button onClick={this.handleFeatching}>Get Weather</button>
+        <button onClick={this.FetchWeather}>Get Weather</button>
       </div>
     );
   }
