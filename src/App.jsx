4f597dea6de85d8025622620a8ec9db48342577c
Abdunesr <nesredinabdelah33@gmail.com>
2024-11-24 22:41:12 +0300

    Adding the date and count functinality to get started with the class based componets of react

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,32 @@ class Count extends React.Component {
   constructor(props) {
     super(props);
     this.state = { Count: 5 };
+    this.handleIncerment = this.handleIncerment.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
   }
+
+  handleIncerment() {
+    this.setState((curCount) => {
+      return { Count: curCount.Count + 1 };
+    });
+  }
+  handleDecrement() {
+    this.setState((curcount) => {
+      return { Count: curcount.Count - 1 };
+    });
+  }
+
   // Fixed typo here
   render() {
+    const date = new Date("june  27  2024");
+    date.setDate(date.getDate() + this.state.Count);
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.Count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          [{date.toDateString()}] {this.state.Count}
+        </span>
+        <button onClick={this.handleIncerment}>+</button>
       </div>
     );
   }
